3ecca28a9dafd197a2e5d96ed192cc1ca3fb8bae
Kushagra Agarwal <kushagraagarwal214@gmail.com>
2021-07-21 00:08:48 +0530

    PreOrder traversal

---
+++ PreOrder.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int k){
+        data=k;
+        left=null;
+        right=null;
+    }
+}
+
+public class PreOrder{
+ 
+    public static void traverse(Node root){
+        if(root==null)
+            return;
+        System.out.print(root.data+" ");
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    public static void main(String args[]){
+
+        //             10
+
+        //     20             30
+
+        // 40      50    60        70
+
+        //     80
+
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+        root.right.left = new Node(60);
+        root.right.right = new Node(70);
+        root.left.right.left = new Node(80);
+
+        traverse(root);
+
+    }
+}
