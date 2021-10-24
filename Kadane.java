81781124493315023e955d1617092fb7d6641b58
Kushagra Agarwal <kushagraagarwal214@gmail.com>
2021-10-24 16:45:06 +0530

    Update Kadane.java

--- Kadane.java
+++ Kadane.java
@@ -4,20 +4,7 @@ import java.util.*;
 public class Kadane{
 
 
-	static void fun(int[] a){
-
-		int i,maxSum=Integer.MIN_VALUE,currSum=0;
-		int n = a.length;
-
-		for(i=0;i<n;i++){
-			currSum+=a[i];
-			if(currSum<0)
-				currSum=0;
-			maxSum = Math.max(maxSum,currSum);
-		}
-
-		System.out.println("Maximum subarray sum is "+maxSum);
-	}
+	
 
 	public static void main(String[] args) {
 
